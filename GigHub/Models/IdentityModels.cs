a413959cc8e90e67e56c04e7baf658107006a186
eduana <eduanath@gmail.com>
2019-05-08 15:58:00 +0200

    Gig And Genre Model Added

--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GigHub.Models
 {
@@ -25,6 +25,8 @@ namespace GigHub.Models
         {
         }
 
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
